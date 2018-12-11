b7535b5ae39f2551b9884547373aa275ffe67ce1
richi2050 <ricardo.lugo.recillas@gmail.com>
2018-12-11 13:18:29 -0600

    creo el componenet media  con estilos y s emostros en pagina principal

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
+import './Media.css';
 
 export default class Media extends Component {
   render() {
+    
     return (
-      <div>
-          <div>
-              <img src="" alt="" width={260} height={160}/>
-              <h3>Titulo</h3>
-              <p>nombre del autor</p>
+      <div className='Media'>
+          <div className="Media-cover">
+              <img src="img/media_1.jpg" alt="" width={260} height={160} className="Media-image"/>
+              <h3 className="Media-title">Titulo</h3>
+              <p className="Media-author">nombre del autor</p>
           </div>
       </div>
     )
