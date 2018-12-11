dcfa1e5173680f769f60acc29140432affa8fa8f
richi2050 <ricardo.lugo.recillas@gmail.com>
2018-12-11 11:59:48 -0600

    Se agrego el componente media

---
+++ src/components/Media.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Media extends Component {
+  render() {
+    return (
+      <div>
+          <div>
+              <img src="" alt="" width={260} height={160}/>
+              <h3>Titulo</h3>
+              <p>nombre del autor</p>
+          </div>
+      </div>
+    )
+  }
+}
