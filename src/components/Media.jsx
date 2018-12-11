c8e91e6a5068d8daf1e2ed23322143bad63efc49
richi2050 <ricardo.lugo.recillas@gmail.com>
2018-12-11 13:23:32 -0600

    se agregaron imagenes del cover

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -7,7 +7,7 @@ export default class Media extends Component {
     return (
       <div className='Media'>
           <div className="Media-cover">
-              <img src="img/media_1.jpg" alt="" width={260} height={160} className="Media-image"/>
+              <img src="img/covers/bitcoin.jpg" alt="" width={260} height={160} className="Media-image"/>
               <h3 className="Media-title">Titulo</h3>
               <p className="Media-author">nombre del autor</p>
           </div>
