205f30c56e7b01f49032849b6d5198d0293befe8
richi2050 <ricardo.lugo.recillas@gmail.com>
2018-12-11 15:23:21 -0600

    enlaces de evento onclick con emas6 y emas7

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
 import './Media.css';
 import PropTypes from 'prop-types';
+
 class Media extends Component {
+  /*inicia forma de  asignar el vento a los props con EMAS6*/ 
+  /*
+  constructor(props){
+    super(props)
+    this.handleClick = this.handleClick.bind(this);
+  } 
+  */
+  /*fin  forma de  asignar el vento a los props con EMAS6*/ 
+
+  handleClick = (event) => {
+    console.log(this.props);
+  }
  
   render() {
-    
     return (
-      <div className='Media'>
+      <div className='Media' onClick={this.handleClick}>
           <div className="Media-cover">
               <img src={this.props.image} alt="" width={260} height={160} className="Media-image"/>
               <h3 className="Media-title">{ this.props.title}</h3>
