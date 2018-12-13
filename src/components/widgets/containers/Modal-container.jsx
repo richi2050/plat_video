35f419216f043e50e54af63d4edf13d1bd794bda
richi2050 <ricardo.lugo.recillas@gmail.com>
2018-12-13 13:08:26 -0600

    se cambio el nombre del contenedor  del modal

--- src/components/widgets/containers/Modal-container.jsx
+++ src/components/widgets/containers/Modal-container.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import {createPortal} from 'react-dom'
 
-export default class Modal extends React.Component {
+export default class ModalContainer extends React.Component {
   render() {
     return createPortal(
       this.props.children,
-      document.getElementById('modal')
+      document.getElementById('modal-container')
     )
   }
 }
